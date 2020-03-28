750fd3cbee9f552913fe25b2c51f26b5f782ab74
statnmap <sebastienrochettefr@gmail.com>
2020-03-28 15:49:46 +0100

    First release

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# rcpp.pkg
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of rcpp.pkg is to test a reprex package with Rcpp
+
+## Installation
+
+You can install from github with
+
+``` r
+# install.packages("remotes")
+remotes::install_github("statnmap/rcpp.pkg")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(rcpp.pkg)
+## basic example code
+```
+
---
+++ dev_history.R
@@ -0,0 +1,43 @@
+# Rcpp.package.skeleton("mypackage")
+
+# instructions Rcpp
+# * Edit the help file skeletons in 'man', possibly combining help files for
+# multiple functions.
+# * Edit the exports in 'NAMESPACE', and add necessary imports.
+# * Put any C/C++/Fortran code in 'src'.
+# * If you have compiled code, add a useDynLib() directive to 'NAMESPACE'.
+# * Run R CMD build to build the package tarball.
+# * Run R CMD check to check the package tarball.
+#
+# Read "Writing R Extensions" for more information.
+
+
+# Hide dev_history from build
+usethis::use_build_ignore("dev_history.R")
+
+# Check package
+devtools::check()
+
+# Load package for test
+devtools::load_all()
+
+# Test function
+rcpp_hello_world()
+
+# Add Readme for github
+usethis::use_readme_md()
+
+# Init git to share this package
+# Configure your git
+usethis::use_git_config(
+  scope = "project",
+  user.name = "Your Name",
+  user.email = "you@example.com"
+)
+
+# library is protected by a .gitignore inside renv/ directory
+usethis::use_git()
+#> commit
+
+# Send to github
+
